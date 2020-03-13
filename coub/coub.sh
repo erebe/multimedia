6b39dbe3ca356e5e5f35d4268533cfd97482dbc9
Romain GERARD <r.gerard@criteo.com>
2020-03-13 22:41:26 +0100

    bump

--- coub/coub.sh
+++ coub/coub.sh
@@ -10,13 +10,13 @@ youtube-dl -f html5-audio-high -o ${name}.mp3 "$1"
 printf '\x00\x00' | dd of=${name}_.mp4 bs=1 count=2 conv=notrunc
 for i in `seq 1 "$loops"`; do echo "file '${name}_.mp4'" >> ${name}.txt; done
 
-ffmpeg -hide_banner -t 10 -f concat -i ${name}.txt -i ${name}.mp3 -c copy -shortest -movflags faststart "$name".mp4
+ffmpeg -hide_banner -t 30 -f concat -i ${name}.txt -i ${name}.mp3 -c copy -shortest -movflags faststart "$name".mp4
 
 # Version without watermark
 echo '' > "${name}.txt"
 venv/bin/python remove_watermark.py "${name}_.mp4"
 for i in `seq 1 "$loops"`; do echo "file '${name}_.mp4.avi'" >> ${name}.txt; done
-ffmpeg -hide_banner -t 10 -f concat -i ${name}.txt -i ${name}.mp3 -c copy -shortest -movflags faststart "$name".mp4.avi
+ffmpeg -hide_banner -t 30 -f concat -i ${name}.txt -i ${name}.mp3 -c copy -shortest -movflags faststart "$name"__.mp4
 
 rm ${name}_.mp4 ${name}.mp3 ${name}.txt ${name}_.mp4.avi
 
